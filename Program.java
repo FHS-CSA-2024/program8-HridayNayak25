512ce807cf1a725d27f4e86e8b8b33c806fef4ab
HridayNayak25 <hridaynayak8@gmail.com>
2024-09-06 18:08:44 -0700

    Update Program.java
    Program 8 final code

--- Program.java
+++ Program.java
@@ -1,9 +1,50 @@
 //import stuff here
-
+import java.lang.Math;
 //Your code here
+public class math{
+    public double number1;
+    public double number2;
+    public double list;
+    
+    
+    public String math(double number1, double number2){
+        String output = "";
+        double sum = number1 + number2;
+        double diff = number1 - number2;
+        double product = number1 * number2;
+        double average = ((number1 + number2)/2);
+        double absVal = Math.abs(diff);
+        double maxVal = Math.max(number1,number2);
+        double minVal = Math.min(number1,number2);
+        
+        output = ("Sum: " + sum + "Difference: " + diff + "Product: "+ product +
+        "Average: " + average + "Absoulute: " + absVal + "Maximum: " + maxVal +
+        "Minimum: " + minVal);
+        
+        return output;
+        
+        
+        
+        
+    }
+    
+       
+    
+
+
+
+
+
+
+
+
+
+}
+
 
 //Paste console output below:
 /*
-
+    System.out.println(output);
 
 */
+
